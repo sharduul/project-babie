dd862970e414e8587eadc8dfc9ff5210aff5e7c7
sharduul <bagadeshardul@gmail.com>
2015-03-15 19:26:53 -0400

    get all names api implemented

--- babie-service/src/api/name.js
+++ babie-service/src/api/name.js
@@ -3,18 +3,32 @@ var Name = require('../models').nameModel;
 
 module.exports = function(app){
 
+	// get all the names
 	app.get('/api/name', function(req, res){
 
-		var testName = {
-			nameId: 1,
-			nameInfo: "test success"
-		};
-
-		Name.create(testName, function(err){
-			console.log("error: " + err);
+		Name.find({}, function(err, names){
+			
+			res.send(names); // send array of names to the client
 		});
 
 		console.log("success /name");
 	});
+
+
+
+	// for reference.. will be needed in the future..
+	// app.post('/api/name', function(req, res){
+
+
+		// var testName = {
+		// 	nameId: 1,
+		// 	nameInfo: "test success"
+		// };
+
+	// 	Name.create(testName, function(err){
+	// 		console.log("error: " + err);
+	// 	});
+
+	// });
 
 };
