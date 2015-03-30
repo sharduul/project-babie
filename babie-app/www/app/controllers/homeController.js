7d2d89d5c65963c4478f2cea78f6ef940335673f
sharduul <bagadeshardul@gmail.com>
2015-03-29 22:42:57 -0400

    V IMP checkin- now running on my HTC ONE... some weird runtime error had struck.. may be was missing some platform file.. so created new babie project, tried running that in step by step manner.. then finally when everything was ported- copy pasted all the files from new babie project to this repo's babie project... including nodemodules platform hooks, etc.. everything!

--- babie-app/www/app/controllers/homeController.js
+++ babie-app/www/app/controllers/homeController.js
@@ -14,12 +14,110 @@
 
 		vm.testText = '...still the home page!!!';
 
-		apiResource.name.name().get(function(result){
+		//apiResource.name.name().get(function(result){
+
+		var result = [
+		{
+			"nameId": 1,
+			"nameInfo": "vijay",
+			"meaning": [
+				{
+					"meaningId": 1,
+					"meaningInfo": "victory"
+				},
+				{
+					"meaningId": 1,
+					"meaningInfo": "victory"
+				}
+			]
+			
+		},
+
+		{
+			"nameId": 1,
+			"nameInfo": "vijay",
+			"meaning": [
+				{
+					"meaningId": 1,
+					"meaningInfo": "victory"
+				},
+				{
+					"meaningId": 1,
+					"meaningInfo": "victory"
+				}
+			]
+			
+		},
+
+		{
+			"nameId": 1,
+			"nameInfo": "vijay",
+			"meaning": [
+				{
+					"meaningId": 1,
+					"meaningInfo": "victory"
+				},
+				{
+					"meaningId": 1,
+					"meaningInfo": "victory"
+				}
+			]
+			
+		},
+
+		{
+			"nameId": 1,
+			"nameInfo": "vijay",
+			"meaning": [
+				{
+					"meaningId": 1,
+					"meaningInfo": "victory"
+				},
+				{
+					"meaningId": 1,
+					"meaningInfo": "victory"
+				}
+			]
+			
+		},
+
+		{
+			"nameId": 1,
+			"nameInfo": "vijay",
+			"meaning": [
+				{
+					"meaningId": 1,
+					"meaningInfo": "victory"
+				},
+				{
+					"meaningId": 1,
+					"meaningInfo": "victory"
+				}
+			]
+			
+		},
+
+		{
+			"nameId": 1,
+			"nameInfo": "vijay",
+			"meaning": [
+				{
+					"meaningId": 1,
+					"meaningInfo": "victory"
+				},
+				{
+					"meaningId": 1,
+					"meaningInfo": "victory"
+				}
+			]
+			
+		},
+	]
 
 			vm.names = result;
 
-			console.log(result);
-		});
+			//console.log(result);
+		//});
 
 
 	}
