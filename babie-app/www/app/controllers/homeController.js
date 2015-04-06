d4b0cbcdfc3a12b70ca46c2e91469cacf1a7bd85
Shardul Bagade <shardul.bagade@energycap.com>
2015-04-06 17:07:16 -0400

    14: names object used to display info on md-card

--- babie-app/www/app/controllers/homeController.js
+++ babie-app/www/app/controllers/homeController.js
@@ -14,6 +14,16 @@
 
 		vm.testText = '...still the home page!!!';
 
+		// constructor
+		(function(){
+
+			// NEED IT: when names api is completely implemented
+			// apiResource.name.name().get({}, function(result){
+			// 	names = result;
+			// });
+
+		})();
+
 		//apiResource.name.name().get(function(result){
 
 		var result = [
@@ -23,11 +33,66 @@
 			"meaning": [
 				{
 					"meaningId": 1,
-					"meaningInfo": "victory"
+					"meaningInfo": "A person asdfasd fdfasff  asdfdafsdaf afasdfsadfads asfasf asfaaf fasdfdasfdas"
+				},
+				{
+					"meaningId": 2,
+					"meaningInfo": "A person asdfasd fdfasff  asdfdafsdaf afasdfsadfads asfasf asfaaf fasdfdasf"
+				}
+			],
+			"labels": [
+				{
+					"labelId": 1,
+					"labelInfo": "god"
+				},
+				{
+					"labelId": 2,
+					"labelInfo": "boy"
+				}
+			]
+			
+		},
+
+		{
+			"nameId": 1,
+			"nameInfo": "vijay",
+			"meaning": [
+				{
+					"meaningId": 1,
+					"meaningInfo": "A person asdfasd fdfasff  asdfdafsdaf afasdfsadfads asfasf asfaaf fasdfdasfdasasasfasdf faddasffa dsfd sfsdfears"
 				},
 				{
 					"meaningId": 1,
 					"meaningInfo": "victory"
+				}
+			],
+			"labels": [
+				{
+					"labelId": 1,
+					"labelInfo": "god"
+				},
+				{
+					"labelId": 2,
+					"labelInfo": "boy"
+				}
+			]
+			
+		},
+
+		{
+			"nameId": 1,
+			"nameInfo": "vijay",
+			"meaning": [
+				
+			],
+			"labels": [
+				{
+					"labelId": 1,
+					"labelInfo": "god"
+				},
+				{
+					"labelId": 2,
+					"labelInfo": "boy"
 				}
 			]
 			
@@ -45,73 +110,21 @@
 					"meaningId": 1,
 					"meaningInfo": "victory"
 				}
-			]
-			
-		},
-
-		{
-			"nameId": 1,
-			"nameInfo": "vijay",
-			"meaning": [
+			],
+			"labels": [
 				{
-					"meaningId": 1,
-					"meaningInfo": "victory"
+					"labelId": 1,
+					"labelInfo": "god"
 				},
 				{
-					"meaningId": 1,
-					"meaningInfo": "victory"
+					"labelId": 2,
+					"labelInfo": "boy"
 				}
 			]
 			
-		},
+		}
 
-		{
-			"nameId": 1,
-			"nameInfo": "vijay",
-			"meaning": [
-				{
-					"meaningId": 1,
-					"meaningInfo": "victory"
-				},
-				{
-					"meaningId": 1,
-					"meaningInfo": "victory"
-				}
-			]
-			
-		},
-
-		{
-			"nameId": 1,
-			"nameInfo": "vijay",
-			"meaning": [
-				{
-					"meaningId": 1,
-					"meaningInfo": "victory"
-				},
-				{
-					"meaningId": 1,
-					"meaningInfo": "victory"
-				}
-			]
-			
-		},
-
-		{
-			"nameId": 1,
-			"nameInfo": "vijay",
-			"meaning": [
-				{
-					"meaningId": 1,
-					"meaningInfo": "victory"
-				},
-				{
-					"meaningId": 1,
-					"meaningInfo": "victory"
-				}
-			]
-			
-		},
+		
 	]
 
 			vm.names = result;
