7938c88786577e12f8c5189881811a47387a86b0
sharduul <bagadeshardul@gmail.com>
2015-04-11 00:28:05 -0400

    14: names and meaning finally updated in the database with the CORRECT SCHEMA. need to clean the names.js

--- babie-service/src/api/name.js
+++ babie-service/src/api/name.js
@@ -1,15 +1,19 @@
 
 var Q = require('q');
+var Q = require('q');
+var _ = require('lodash');
 
 var Name = require('../models').nameModel;
 var OldName = require('../models').oldNameModel;
 var Meaning = require('../models').meaningModel;
 //var Counter = require('../models').counterModel;
 
-module.exports = function(app, counter){
+module.exports = function(app, counter, dbqueries){
 
 	var nameToBeSavedCollection = [];
+	var nameToBeSavedCollection2 = [];
 	var meaningToBeSavedCollection = [];
+	var meaningToBeSavedCollection2 = [];
 
 	var PushName;
 	var SaveNames;
@@ -25,14 +29,54 @@ module.exports = function(app, counter){
 			var names = JSON.parse(namesStr);
 			names = FlatenNameList(names);
 
-			SaveNames(names)
-				.then(function(){
-					console.log(nameToBeSavedCollection.length);
-					console.log("success /name");
-				});
+			GetNamesToSave(names)
+			.then(function(){
+
+				//console.log(_.find);
+				//console.log(_);
+
+				console.log("inserting... " + nameToBeSavedCollection.length);
+
+				//var promise = Name.create(nameToBeSavedCollection);
+
+				//var potatoBag = [{nameId: 1}, {nameId: 2}];
+				Name.collection.insert(nameToBeSavedCollection, onInsert);
+				Name.collection.insert(nameToBeSavedCollection2, onInsert);
+				Meaning.collection.insert(meaningToBeSavedCollection, onInsert);
+				Meaning.collection.insert(meaningToBeSavedCollection2, onInsert);
 
 
-			//console.log("success /name");
+
+				function onInsert(err, docs) {
+				    if (err) {
+				        console.log(err);
+				    } else {
+				        console.info('%d potatoes were successfully stored.', docs.length);
+				    }
+				}
+
+				// promise.then(function(){
+
+				// 	console.log("asdf");
+				// });
+
+				// dbqueries.SaveCollection(Name, nameToBeSavedCollection).exec()
+				// .then(function(data){
+
+				// 	console.log(data);
+				// 	console.log("success /name");
+
+				// });
+
+				// dbqueries.SaveCollection(Meaning, meaningToBeSavedCollection).exec()
+				// .then(function(data){
+
+				// 	console.log(data);
+				// 	console.log("success /meaning");
+
+				// });
+			});
+
 		});
 
 	});
@@ -57,7 +101,7 @@ module.exports = function(app, counter){
 // ********************* private functions **********************************************
 
 
-	PushName = function(singleName){
+	PushName = function(singleName, count){
 
 		var defer = Q.defer();
 
@@ -76,7 +120,7 @@ module.exports = function(app, counter){
 			seqMeaningId = data[0].seq;
 			nameToBeSaved = {};
 
-			nameToBeSaved.nameID = seqNameId;
+			nameToBeSaved.nameId = seqNameId;
 			nameToBeSaved.nameInfo = singleName.name;
 			nameToBeSaved.gender = singleName.gender;
 
@@ -85,9 +129,42 @@ module.exports = function(app, counter){
 				meaningInfo: singleName.meaning
 			}
 
-			nameToBeSavedCollection.push(nameToBeSaved);
-			meaningToBeSavedCollection.push(nameToBeSaved.meaning);
+			var found = _.find(nameToBeSavedCollection, 'nameId', seqNameId);
+			var found2 = _.find(nameToBeSavedCollection2, 'nameId', seqNameId);
+			var foundM = _.find(meaningToBeSavedCollection, 'meaningId', seqMeaningId);
+			var foundM2 = _.find(meaningToBeSavedCollection2, 'meaningId', seqMeaningId);
 
+			if(typeof found === 'undefined' && typeof found2 === 'undefined' && typeof foundM === 'undefined' && typeof foundM2 === 'undefined'){
+
+				//console.log(found);
+			
+
+				// if(typeof found === 'undefined'){
+
+					
+				// 	console.log("found it");
+				// }
+
+				if(count % 2 == 0){
+					nameToBeSavedCollection.push(nameToBeSaved);
+				} else {
+					//console.log("filled names 1");
+					nameToBeSavedCollection2.push(nameToBeSaved);
+				}
+
+				if(count % 2 == 0){
+					meaningToBeSavedCollection.push(nameToBeSaved.meaning);
+				} else {
+					//console.log("filled meanings 1");
+
+					meaningToBeSavedCollection2.push(nameToBeSaved.meaning);
+				}
+
+
+
+			}
+
+			
 			defer.resolve();
 
 		});
@@ -97,13 +174,16 @@ module.exports = function(app, counter){
 	}
 
 	
-	var SaveNames = function(names){
+	var GetNamesToSave = function(names){
 
 		var promises = [];
 		var defer = Q.defer();
 
+		var count = 0;
+
 		names.forEach(function(singleName){
-			promises.push(PushName(singleName))
+			count++;
+			promises.push(PushName(singleName, count))
 		});
 
 		Q.all(promises).then(function(){
