67208cc1359176fb0e55270fe6c7dbf94758cc5f
Shardul Bagade <shardul.bagade@energycap.com>
2015-09-17 11:42:29 -0400

    cleaned up gulp

--- babie-app/gulpfile.js
+++ babie-app/gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
 var concat = require('gulp-concat');
-var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
@@ -14,7 +13,7 @@ var paths = {
   less: ['./less/**/*.less']
 };
 
-gulp.task('default', ['sass', 'less']);
+gulp.task('default', ['less']);
 
 gulp.task('less', function(){
   gulp.src('./less/main.less')
@@ -35,7 +34,7 @@ gulp.task('sass', function(done) {
 });
 
 gulp.task('watch', function() {
-  gulp.watch([paths.sass, paths.less], ['sass', 'less']);
+  gulp.watch([paths.sass, paths.less], ['less']);
 });
 
 gulp.task('install', ['git-check'], function() {
