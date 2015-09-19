ee285c553e7057c0ea5b2336dedd3810855a94b7
Shardul <bagadeshardul@gmail.com>
2015-09-19 19:02:21 -0400

    24: API to get a name by name Id implemented

--- babie-service/src/api/name.js
+++ babie-service/src/api/name.js
@@ -1,19 +1,31 @@
-
 var Name = require('../models').nameModel;
 
 module.exports = function(app, counter, dbqueries){
 
+    // API to get a name by nameId
+    app.get('/api/name/:nameId', function(req, res){
 
-	// get all the names
+        Name.find({nameId: req.params.nameId}, function(err, name){
+            if(err){
+                return res.send(err);
+            }
+            res.send(name);
+            console.log("success /:nameId");
+        });
+    });
+
+
+	// API to get all the names
 	app.get('/api/name', function(req, res){
 
 		Name.find({}, function(err, names){
-			
-				res.send(names);
-
-			});
-
-		});
+            if(err){
+                return res.send(err);
+            }
+            res.send(names);
+            console.log("success /name");
+        });
+    });
 
 	
 
