1004762832d201011cbcd83456d3b9df1dea8e13
Shardul <bagadeshardul@gmail.com>
2016-08-14 23:00:46 -0400

    iOS platform added

--- babie-app/www/components/home/homeController.js
+++ babie-app/www/components/home/homeController.js
@@ -16,19 +16,22 @@
 
 		(function(){
 
-			// get all the names alphabetically
-            // call the API for each alphabet
-            var index = -1;
-            angular.forEach(alphabetArray, function(currentAlphabet){
-                apiResource.name.name().get({filter: "search equals " + currentAlphabet, page: 1, size: 5}, function(result){
-                    index++;
-                    // TODO: for sorting the map alphabetically use map['a'], map['b'], etc..
-                    vm.alphabeticNames[index] = result;
-                });
+        // get all the names alphabetically
+        // call the API for each alphabet
+        var index = -1;
+        angular.forEach(alphabetArray, function(currentAlphabet){
+            apiResource.name.name().get({filter: "search equals " + currentAlphabet, page: 1, size: 10}, function(result){
+                index++;
+                // TODO: for sorting the map alphabetically use map['a'], map['b'], etc..
+                vm.alphabeticNames[index] = result;
+
+                console.log(vm.alphabeticNames[0]);
+
             });
+        });
 
 		})();
-		
+
 	}
 
 })();
