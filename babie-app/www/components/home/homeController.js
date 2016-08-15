8f8a2dfd56a4ece8b455bb9cbd2721f0a41687ee
Shardul <bagadeshardul@gmail.com>
2016-08-14 23:55:34 -0400

    toolbar search added

--- babie-app/www/components/home/homeController.js
+++ babie-app/www/components/home/homeController.js
@@ -10,16 +10,18 @@
 
 	function homeController(apiResource, helperService) {
 		var vm = this;
-		var alphabetArray = helperService.alphabetString().split('');
 
 		vm.alphabeticNames = [];
+		vm.alphabetArray = helperService.alphabetString().split('');
 
 		(function(){
+
+      console.log(vm.alphabetArray);
 
         // get all the names alphabetically
         // call the API for each alphabet
         var index = -1;
-        angular.forEach(alphabetArray, function(currentAlphabet){
+        angular.forEach(vm.alphabetArray, function(currentAlphabet){
             apiResource.name.name().get({filter: "search equals " + currentAlphabet, page: 1, size: 10}, function(result){
                 index++;
                 // TODO: for sorting the map alphabetically use map['a'], map['b'], etc..
