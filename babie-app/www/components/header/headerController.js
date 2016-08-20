c9614e12dd37b85fb3628c54fc51ebd2f563941e
Shardul <bagadeshardul@gmail.com>
2016-08-20 00:40:30 -0400

    search box hooked up to the API

--- babie-app/www/components/header/headerController.js
+++ babie-app/www/components/header/headerController.js
@@ -6,23 +6,49 @@
 		.module('babie.components.header', ['ionic'])
 		.controller('headerController', headerController);
 
-	headerController.$inject = [];
+	headerController.$inject = ['$q', '$timeout', 'apiResource'];
 
-	function headerController() {
+	function headerController($q, $timeout, apiResource) {
 		var vm = this;
+    var searchTimer = null;
 
 		vm.searchActivated = false;
-		vm.toggleSearch = toggleSearch;
+    vm.searchText = "";
 
-        (function(){
+    vm.toggleSearch = toggleSearch;
+    vm.querySearch = querySearch;
 
-			console.log("header");
+    (function(){
+
 
 		})();
 
 		function toggleSearch(){
+      vm.searchText = "";
 			vm.searchActivated = !vm.searchActivated;
 		}
+
+    // takes care of search functionality
+    function querySearch(query){
+
+      // have to return promise first- to the autocomplete box.. so that it will wait for the results
+      var defer = $q.defer();
+
+      // user is still typing.. thus cancel the timer
+      if(searchTimer){
+        $timeout.cancel(searchTimer);
+      }
+
+      // search by search-term
+      searchTimer = $timeout(function(){
+        apiResource.name.name().get({filter: "search equals " + query, page: 1, size: 10}, function(result){
+          defer.resolve(result);
+        });
+
+      }, 500);
+
+      return defer.promise;
+    }
 	}
 
 })();
