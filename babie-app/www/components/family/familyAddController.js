7cd2cf2760acf18d26003be6e04a769009b0db8f
Shardul <bagadeshardul@gmail.com>
2016-08-21 00:11:06 -0400

    add family view added

--- babie-app/www/components/family/familyAddController.js
+++ babie-app/www/components/family/familyAddController.js
@@ -11,11 +11,77 @@
 	function familyAddController($q, $timeout, apiResource) {
 		var vm = this;
 
+    vm.family = {
+      membersList: []
+    };
+
+    vm.querySearch = querySearch;
+
     (function(){
 
       console.log("adsfs");
 
 		})();
+
+
+    function querySearch(query){
+      var results = [];
+      if(query){
+        results = _.filter(vm.userConnections, function(connectionItem){
+
+          // filter out connections which are already in the chips
+          if(_.find(vm.family.membersList, { 'userIdHash': connectionItem.userIdHash })){
+            return false;
+          }
+
+          // return this connection as a new member
+          else if(connectionItem.name && connectionItem.name.toLowerCase().indexOf(query.toLowerCase()) > -1){
+            connectionItem.isNewMember = true;
+            return true;
+          }
+
+          return false;
+        });
+      }
+
+      return results;
+    }
+
+
+    vm.userConnections = [
+      {
+        email: "abc1@abc.com",
+        name: "name 1"
+      },
+      {
+        email: "abc2@abc.com",
+        name: "name 2"
+      },
+      {
+        email: "abc3@abc.com",
+        name: "name 3"
+      },
+      {
+        email: "abc4@abc.com",
+        name: "name 4"
+      },
+      {
+        email: "abc1@abc.com",
+        name: "name 1"
+      },
+      {
+        email: "abc2@abc.com",
+        name: "name 2"
+      },
+      {
+        email: "abc3@abc.com",
+        name: "name 3"
+      },
+      {
+        email: "abc4@abc.com",
+        name: "name 4"
+      }
+    ];
 
 	}
 
