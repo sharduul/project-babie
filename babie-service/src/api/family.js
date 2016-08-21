3cca02ac590ac53db7bc4c41b48c0bf127bc2cf4
Shardul <bagadeshardul@gmail.com>
2016-08-21 13:35:46 -0400

    test deploy: trigger build

--- babie-service/src/api/family.js
+++ babie-service/src/api/family.js
@@ -35,7 +35,7 @@ module.exports = function(app, dbqueries){
 // ********************** sample data for testing ***********************
 
     var testFamily = {
-        "familyId": 12336,
+        "familyId": 12337,
         "familyName": "first family",
         "memberList": [
             {
