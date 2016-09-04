1b75f9bae1841b3adb84b92f25d7f9a4bd8ee51d
Shardul <bagadeshardul@gmail.com>
2016-09-04 11:14:10 -0400

    get user by Id

--- babie-service/src/api/user.js
+++ babie-service/src/api/user.js
@@ -2,11 +2,22 @@ var User = require('../models').userModel;
 
 module.exports = function(app, dbqueries){
 
+    // API to get user by Id
+    app.get('/api/user/:userId', function(req, res){
+
+        User.findOne({ userId: req.params.userId }, function(err, users){
+            if(err){
+                return res.send(err);
+            }
+            res.send(users);
+            console.log("success families");
+        });
+    });
 
     // API to get ALL users
     app.get('/api/user', function(req, res){
 
-        User.find({}, function(err, users){
+        User.find({ }, function(err, users){
             if(err){
                 return res.send(err);
             }
