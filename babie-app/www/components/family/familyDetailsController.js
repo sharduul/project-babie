eeef806927db7bcc6178698f300dc1dc7eee8f8b
Shardul <bagadeshardul@gmail.com>
2016-09-11 02:57:58 -0400

    remove member

--- babie-app/www/components/family/familyDetailsController.js
+++ babie-app/www/components/family/familyDetailsController.js
@@ -6,9 +6,9 @@
 		.module('babie.components.family')
 		.controller('familyDetailsController', familyDetailsController);
 
-  familyDetailsController.$inject = ['$q', '$timeout', 'apiResource', '$ionicSlideBoxDelegate', '$state'];
+  familyDetailsController.$inject = ['apiResource', '$ionicSlideBoxDelegate', '$state'];
 
-	function familyDetailsController($q, $timeout, apiResource, $ionicSlideBoxDelegate, $state) {
+	function familyDetailsController(apiResource, $ionicSlideBoxDelegate, $state) {
 		var vm = this;
     var familyId = $state.params.familyId;
 
@@ -17,14 +17,13 @@
     vm.$ionicSlideBoxDelegate = $ionicSlideBoxDelegate;
 
     vm.slideChanged = slideChanged;
+    vm.removeMember = removeMember;
 
     (function(){
 
       apiResource.family.family().get({ familyId: familyId }, function(result){
-        console.log(result);
         vm.family = result;
       });
-
 
 		})();
 
@@ -32,9 +31,19 @@
       vm.slideIndex = index;
     }
 
+    function removeMember(memberParam){
+      vm.family.memberList = _.filter(vm.family.memberList, function(member){
+        return member.memberId !== memberParam.memberId;
+      });
 
-    vm.babyNames = [{"_id":"5528a260b77373e106cd386c","nameId":1536,"nameInfo":"Om","gender":"boy","labels":[],"meaning":[{"meaningId":308,"meaningInfo":"Omkar; Creation; The essence of life","_id":"5528a260b77373e106cd3f04"}]},{"_id":"5528a260b77373e106cd3684","nameId":1537,"nameInfo":"Om Prakash","gender":"boy","labels":[],"meaning":[{"meaningId":672,"meaningInfo":"The light of creation; Light of the essence of life","_id":"5528a260b77373e106cd3d1c"}]},{"_id":"5528a260b77373e106cd3775","nameId":1538,"nameInfo":"Omkar","gender":"boy","labels":[],"meaning":[{"meaningId":1399,"meaningInfo":"Beginning of Life; Creator of OM","_id":"5528a260b77373e106cd3e0d"}]},{"_id":"5528a260b77373e106cd3813","nameId":468,"nameInfo":"Oorja","gender":"girl","labels":[],"meaning":[{"meaningId":1185,"meaningInfo":"Energy","_id":"5528a260b77373e106cd3eab"}]},{"_id":"5528a260b77373e106cd3438","nameId":469,"nameInfo":"Oormila","gender":"girl","labels":[],"meaning":[{"meaningId":1550,"meaningInfo":"Daughter of King Janaka of Mithila; The youger sister of Sita; Name of Lakshman's wife","_id":"5528a260b77373e106cd3ad0"}]}];
+      updateFamily();
+    }
 
+    function updateFamily(){
+      apiResource.family.family().update({ familyId: familyId }, vm.family, function(result){
+        vm.family = result;
+      });
+    }
 
   }
 
